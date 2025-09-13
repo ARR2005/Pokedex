52d53fddaa16a929dc85b4fac8908af5c621aaf9
Aldwin Rich R. Villanueva <AldwinRich90@gmail.com>
2025-09-13 21:36:33 +0800

    added shacnui/card

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
-    <div>
-        
+    <div class=" min-h-screen bg-cyan-100 opacity-20">
+        <section class="pt-24 pb-16 px-6" > 
+            <div class="relative">
+                <div class="absolute inset-0 flex item-center justify-center">
+                    <div class="text-[14rem] bg-gradient-to-b from-yellow-300 to-slate-300 bg-clip-text">
+                        POKEDEX
+                    </div>
+                </div>
+            </div>
+
+            <div clas="relative z-10 text-cente py-20">
+                <h1 class="text-6xl font-black mb-6 text-red-500">
+                    HELLO WORLD
+                </h1>
+            </div>
+        </section>
     </div>
 </template>
 
