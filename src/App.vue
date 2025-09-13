bdfb8d6dfad1ff8afa4ff276e5e0e98b0b802805
Aldwin Rich R. Villanueva <AldwinRich90@gmail.com>
2025-09-14 00:27:39 +0800

    Created loops for api

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,32 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import { Button } from "@/components/ui/button"
+
+import axios from 'axios'
+import { ref, onMounted } from 'vue'
+
+interface Pokemon {
+  name: string
+  url: string
+}
+
+const pokemon = ref<Pokemon[]>([]) // strongly typed
+
+onMounted(() => {
+  axios.get('https://pokeapi.co/api/v2/pokemon')
+    .then(response => {
+      pokemon.value = response.data.results
+    })
+    .catch(error => {
+      console.error(error)
+    })
+})
+
 </script>
 
 <template>
-    <div class=" min-w-screen min-h-screen bg-slate-400 opacity-20">
+    <div class=" min-w-screen min-h-screen bg-slate-400 opacity-100">
         <section class="pt-24 pb-16 px-6" > 
             <div class="relative">
                 <div class="absolute inset-0 flex item-center justify-center">
@@ -24,27 +46,44 @@ import {
                 <h1 class="text-6xl font-black mb-60 text-red-500">
                     HELLO WORLD
                 </h1>
-                <div class="px-50">
-                    <Card>
-                        <CardHeader>
-                        <CardTitle>Card Title</CardTitle>
-                        <CardDescription>Card Description</CardDescription>
-                        </CardHeader>
-                        <CardContent>
-                        Card Content
-                        </CardContent>
-                        <CardFooter>
-                        Card Footer
-                        </CardFooter>
-                    </Card>
-                    
-                </div>
+                <div class="px-6">
+                    <div class="px-6 grid grid-cols-4 grid-rows-auto">
+                        <Card v-for="poke in pokemon" :key="poke.name" class="m-2">
+                            <CardHeader>
+                                <CardTitle>{{ poke.name }}</CardTitle>
+                            </CardHeader>
+                            <CardContent>
+                                {{ poke.url }}
+                            </CardContent>
+                            <CardFooter>
+                                <Button variant="ghost" size="sm" class="m-auto">Outline</Button>
+                            </CardFooter>
+                        </Card>
+                    </div>
+
+            </div>
+
             </div>
         </section>
     </div>
 </template>
 
 
-<style scoped>
 
+
+
+<style scoped>
+    .container {
+  display: grid; 
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr; 
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr; 
+  gap: 0px 0px; 
+  grid-template-areas: 
+    ". . . . . ."
+    ". . . . . ."
+    ". . . . . ."
+    ". . . . . ."
+    ". . . . . ."
+    ". . . . . ."; 
+}
 </style>
