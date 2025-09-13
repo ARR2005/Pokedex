13f7ed52f133662908ec22fc5ed0d5e40413a488
Aldwin Rich R. Villanueva <AldwinRich90@gmail.com>
2025-09-13 22:50:10 +0800

    added shadcnui/card

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
+<script setup lang="ts">
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+</script>
+
 <template>
-    <div class=" min-h-screen bg-cyan-100 opacity-20">
+    <div class=" min-w-screen min-h-screen bg-slate-400 opacity-20">
         <section class="pt-24 pb-16 px-6" > 
             <div class="relative">
                 <div class="absolute inset-0 flex item-center justify-center">
@@ -10,25 +21,29 @@
             </div>
 
             <div clas="relative z-10 text-cente py-20">
-                <h1 class="text-6xl font-black mb-6 text-red-500">
+                <h1 class="text-6xl font-black mb-60 text-red-500">
                     HELLO WORLD
                 </h1>
+                <div class="px-50">
+                    <Card>
+                        <CardHeader>
+                        <CardTitle>Card Title</CardTitle>
+                        <CardDescription>Card Description</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                        Card Content
+                        </CardContent>
+                        <CardFooter>
+                        Card Footer
+                        </CardFooter>
+                    </Card>
+                    
+                </div>
             </div>
         </section>
     </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-    setup () {
-        
-
-        return {}
-    }
-})
-</script>
 
 <style scoped>
 
